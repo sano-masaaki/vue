2da3c0c0c7e0272bbe4c0de93f436a2d1ed2d25a
sano masaaki <m-sano@tandems.co.jp>
2019-05-20 17:50:13 +0900

    initial_commit

---
+++ vendor/bundle/gems/ffi-1.11.0/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/masaaki/my_study/vue/rails-vue-sandbox/vendor/bundle/gems/ffi-1.11.0/ext/ffi_c/libffi/src/x86/ffitarget.h
---
+++ vendor/bundle/ruby/2.4.0/gems/ffi-1.11.0/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/masaaki/my_study/vue/rails-vue-sandbox/vendor/bundle/ruby/2.4.0/gems/ffi-1.11.0/ext/ffi_c/libffi/src/x86/ffitarget.h
